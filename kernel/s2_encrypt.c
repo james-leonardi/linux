aa63c6067dc6366897340bb0cc97a0416259907a
James Leonardi <james.leonardi@stonybrook.edu>
2023-09-14 15:33:10 +0000

    Buffer flush bugfix

--- kernel/s2_encrypt.c
+++ kernel/s2_encrypt.c
@@ -12,20 +12,20 @@ SYSCALL_DEFINE2(s2_encrypt, char __user *, str, int, key)
 
 	/* Check if key is within valid 1-5 range */
 	if (unlikely(key < 1 || key > 5)) {
-		printk(KERN_ERR "[s2_encrypt] Received out-of-bounds key");
+		printk(KERN_ERR "[s2_encrypt] Received out-of-bounds key\n");
 		return EINVAL;
 	}
 
 	/* Get string length */
 	strlen = strnlen_user(str, PAGE_SIZE);
 	if (unlikely(strlen == 0 || strlen > PAGE_SIZE)) {
-		printk(KERN_ERR "[s2_encrypt] Unable to fetch length of given string");
+		printk(KERN_ERR "[s2_encrypt] Unable to fetch length of given string\n");
 		return EINVAL;
 	}
 
 	/* Verify user pointer */
 	if (!access_ok(str, strlen)) {
-		printk(KERN_ERR "[s2_encrypt] Memory access error");
+		printk(KERN_ERR "[s2_encrypt] Memory access error\n");
 		return EINVAL;
 	}
 
@@ -34,7 +34,7 @@ SYSCALL_DEFINE2(s2_encrypt, char __user *, str, int, key)
 	/* Copy string to kernel space */
 	kstr = kmalloc(strlen, GFP_KERNEL);
 	if (unlikely(copy_from_user(kstr, str, strlen))) {
-		printk(KERN_ERR "[s2_encrypt] Failed to copy string from userspace");
+		printk(KERN_ERR "[s2_encrypt] Failed to copy string from userspace\n");
 		kfree(kstr);
 		return EINVAL;
 	}
@@ -45,13 +45,13 @@ SYSCALL_DEFINE2(s2_encrypt, char __user *, str, int, key)
 
 	/* Copy string back to user space */
 	if (unlikely(copy_to_user(str, kstr, strlen))) {
-		printk(KERN_ERR "[s2_encrypt] Failed to copy string to userspace");
+		printk(KERN_ERR "[s2_encrypt] Failed to copy string to userspace\n");
 		kfree(kstr);
 		return EINVAL;
 	}
 
 	/* Print out encrypted string to kernel log */
-	printk(KERN_INFO "%s", kstr);
+	printk(KERN_INFO "%s\n", kstr);
 	kfree(kstr);
 	return 0;
 }
