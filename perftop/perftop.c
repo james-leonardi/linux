2d7527a9df079fb0ca8944c88510f851afade8d0
James Leonardi <james.leonardi@stonybrook.edu>
2023-11-08 21:23:33 +0000

    Create data structures for tracking TSC

--- perftop/perftop.c
+++ perftop/perftop.c
@@ -5,15 +5,77 @@
 #include <linux/seq_file.h>
 #include <linux/kprobes.h>
 #include <linux/sched.h>
+#include <linux/hashtable.h>
+#include <linux/rbtree.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("James Leonardi <james.leonardi@stonybrook.edu>");
 MODULE_DESCRIPTION("CPU Profiler");
 
+/* Define data structure nodes. */
+struct pid_starttsc {
+	pid_t pid;
+	unsigned long start_tsc;
+	struct hlist_node node;
+};
+struct pid_totaltsc {
+	pid_t pid;
+	unsigned long total_tsc;
+	struct rb_node node;
+};
+
 /* Declare global variables. */
 static int pre_count, post_count, context_switch_count;
 static DEFINE_SPINLOCK(pre_count_lock);
 static DEFINE_SPINLOCK(post_count_lock);
+static DECLARE_HASHTABLE(start_tsc, 10); /* Used to translate PID -> start TSC */
+static struct rb_root total_tsc = RB_ROOT; /* Used to keep track of total TSC */
+
+/* Declare global structs. */
+static const struct proc_ops perftop_ops = {
+	.proc_open = perftop_open,
+	.proc_read = seq_read,
+	.proc_lseek = seq_lseek,
+	.proc_release = single_release,
+};
+static struct kretprobe cfs_probe =
+{
+	.entry_handler = entry_pick_next_fair,
+	.handler = ret_pick_next_fair,
+	.data_size = sizeof(struct task_struct),
+};
+
+/** Declare functions. **/
+/* Read 'rdtsc' and return its value. */
+static unsigned long get_rdtsc(void)
+{
+	unsigned long lo, hi;
+	asm volatile ("rdtsc" : "=a" (lo), "=d" (hi));
+	lo |= hi << 32;
+	return lo;
+}
+
+/* Get the entry into the hashtable for the given pid. */
+static struct pid_starttsc *get_pid_starttsc(pid_t pid)
+{
+	struct pid_starttsc *cursor;
+	hash_for_each_possible(start_tsc, cursor, node, pid) {
+		if (cursor->pid == pid)
+			return cursor;
+	}
+	return NULL;
+}
+
+/* Get the entry into the rbtree for the given pid. */
+static struct pid_totaltsc *get_pid_totaltsc(pid_t pid)
+{
+	struct pid_totaltsc *cursor, *temp;
+	rbtree_postorder_for_each_entry_safe(cursor, temp, total_tsc, node) {
+		if (cursor->pid == pid)
+			return cursor;
+	}
+	return NULL;
+}
 
 /* Output of proc file. */
 static int perftop_show(struct seq_file *m, void *v)
@@ -69,27 +131,23 @@ static int ret_pick_next_fair(struct kretprobe_instance *ri, struct pt_regs *reg
 		goto null_task;
 
 	/* Compare PIDs */
-	if (prev_task->pid != next_task->pid)
-		context_switch_count++;
+	if (prev_task->pid == next_task->pid)
+		goto no_context_switch;
+	
+	/* At this point, there has been a context switch.
+	 * Increment the counter and prepare to calculate tsc. */
+	context_switch_count++;
+	//unsigned long current_tsc = get_rdtsc(); /* Timestamp this moment for comparisons. */
+
+	/* Retrieve the prev_task running time using hashtable. */
+	struct pid_starttsc *prev_start = get_pid_starttsc(prev_task->pid);
+
 
 null_task:
+no_context_switch:
 	spin_unlock_irqrestore(&post_count_lock, flags);
 	return 0;
 }
-
-static const struct proc_ops perftop_ops = {
-	.proc_open = perftop_open,
-	.proc_read = seq_read,
-	.proc_lseek = seq_lseek,
-	.proc_release = single_release,
-};
-
-static struct kretprobe cfs_probe =
-{
-	.entry_handler = entry_pick_next_fair,
-	.handler = ret_pick_next_fair,
-	.data_size = sizeof(struct task_struct),
-};
 
 static int __init perftop_init(void)
 {
