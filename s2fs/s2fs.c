25ac717a1903971808305196a6fbaf60313300a3
James Leonardi <james.leonardi@stonybrook.edu>
2023-12-07 15:49:31 +0000

    File ops

--- s2fs/s2fs.c
+++ s2fs/s2fs.c
@@ -12,26 +12,60 @@ MODULE_DESCRIPTION("File System");
 #define S2FS_PAGESHIFT 12
 #define S2FS_PAGESIZE (1UL << S2FS_PAGESHIFT)
 
+static char *msg = "Hello World!";
+
 static struct inode *s2fs_make_inode(struct super_block *sb, int mode)
 {
 	struct inode *inode;
-	struct timespec64 current_time;
+	struct timespec64 time;
 
 	inode = new_inode(sb);
 	if (!inode)
 		return NULL;
-	current_time = current_time(inode);
+	time = current_time(inode);
 
 	inode->i_mode = mode;
 	inode->i_ino = get_next_ino();
-	inode->i_uid = 0;
-	inode->i_gid = 0;
 	inode->i_blocks = 0;
-	inode->i_atime = current_time;
-	inode->i_mtime = current_time;
-	inode->i_ctime = current_time;
+	inode->i_atime = time;
+	inode->i_mtime = time;
+	inode->i_ctime = time;
 
 	return inode;
+}
+
+/* Function unused */
+static int s2fs_open(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+
+/* Function unused */
+static ssize_t s2fs_write(struct file *file, const char __user *buf,
+		size_t count, loff_t *offset)
+{
+	return 0;
+}
+
+static ssize_t s2fs_read(struct file *file, char __user *buf,
+		size_t count, loff_t *offset)
+{
+	unsigned long msg_len = strlen(msg);
+	if (copy_to_user(buf, msg, msg_len))
+		return -EFAULT;
+	return 0;
+}
+
+static struct file_operations s2fs_fops = {
+	.open	= s2fs_open,
+	.write	= s2fs_write,
+	.read	= s2fs_read,
+};
+
+static struct dentry *s2fs_create_file(struct super_block *sb,
+		struct dentry *dir, const char *file_name)
+{
+	return NULL;
 }
 
 static struct dentry *s2fs_create_dir(struct super_block *sb,
@@ -40,7 +74,7 @@ static struct dentry *s2fs_create_dir(struct super_block *sb,
 	struct dentry *dentry;
 	struct inode *inode;
 
-	dentry = d_alloc_name(parent, name);
+	dentry = d_alloc_name(parent, dir_name);
 	if (!dentry)
 		return NULL;
 
@@ -58,7 +92,7 @@ static struct dentry *s2fs_create_dir(struct super_block *sb,
 
 static struct super_operations s2fs_s_ops = {
 	.statfs		= simple_statfs,
-	.drop_inode	= generic_delete_inode
+	.drop_inode	= generic_delete_inode,
 };
 
 static int s2fs_fill_super(struct super_block *sb, void *data, int silent)
@@ -103,7 +137,7 @@ static struct file_system_type s2fs_type = {
 	.owner		= THIS_MODULE,
 	.name		= "s2fs",
 	.mount		= s2fs_get_super,
-	.kill_sb	= kill_anon_super
+	.kill_sb	= kill_anon_super,
 };
 	
 /* Entry/exit functions */
